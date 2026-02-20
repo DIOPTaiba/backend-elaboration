c993848694b025ee67d05a72e669e1e484525a49
missndeya <ndeyethiamastou@gmail.com>
2026-02-20 14:50:26 +0000

    initialisation du projet

---
+++ src/main/java/sn/sysbudgep/elaboration/ServletInitializer.java
@@ -0,0 +1,13 @@
+package sn.sysbudgep.elaboration;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElaborationApplication.class);
+	}
+
+}
