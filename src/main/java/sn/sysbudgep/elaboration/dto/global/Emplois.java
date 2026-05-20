0e943e97e05f604a7b8d80d227f184b0ab56f86f
Dinfo <dinfo@Macintosh.local>
2026-05-20 00:12:54 +0000

    delete api Liste emplois

--- src/main/java/sn/sysbudgep/elaboration/dto/global/Emplois.java
+++
@@ -1,9 +0,0 @@
-package sn.sysbudgep.elaboration.dto.global;
-
-import java.math.BigDecimal;
-
-public interface Emplois {
-    String getIdEmploi();
-    String getCodeEmploi();
-    String getLibEmploi();
-}
